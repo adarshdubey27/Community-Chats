76ae9242911679bdfdbf531e1795de600a596dba
Adarsh Dubey <141320327+adarshdubey27@users.noreply.github.com>
2025-02-13 00:56:26 +0530

    Update server.js

--- project/Community/server.js
+++ project/Community/server.js
@@ -91,7 +91,7 @@ wss.on('connection', (ws) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
